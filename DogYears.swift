042b8ea4a7454dde896879f43149bcc87392f978
Vanessa Santana <vsantana.it@gmail.com>
2021-05-22 21:40:51 -0400

    Create DogYears.swift

---
+++ DogYears.swift
@@ -0,0 +1,8 @@
+var dogAge = 3 //Dog's age as a number
+
+var earlyYears = 21 //First two years of dog's life
+var laterYears = (dogAge - 2) * 4 //Each following year counts as 4 human years
+var humanYears = earlyYears + laterYears //Dog years in HUman years
+
+print("My name is Woof! Ruff ruff, I am \(humanYears) years old in human years.")
+
